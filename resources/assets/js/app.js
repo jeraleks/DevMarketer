280b860658822ebae115df8572bcf326efde29d4
jeraleks <jeraleks@yahoo.com>
2018-04-09 18:44:54 +0200

    Configured CSS, LogIn, Register, Navigation

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,8 +2,19 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-import Buefy from 'buefy'
+import Buefy from 'buefy';
 
 Vue.use (Buefy);
 //Vue.component('example', require('./components/Example.vue'));
 
+var app = new Vue ({
+    el: '#app',
+	data: {}
+})
+
+$(document).ready(function() {
+    //  Dropdowns
+    $('button.dropdown').hover(function(e) {
+      $(this).toggleClass('is-open');
+    });
+  });
